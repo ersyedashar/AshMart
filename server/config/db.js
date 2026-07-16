5c862e0958f7e0182636d4f9c3f26dd467b52c9e
SYED ASHAR <ersyedashar@gmail.com>
2026-07-17 03:46:08 +0530

    fix: retry MongoDB connection instead of crashing server

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`MongoDB error: ${err.message}`);
-    process.exit(1);
+    setTimeout(connectDB, 5000);
   }
 };
 
